cd8911ce61ee1e1bf052fe8b9103855d88f1072a
TjarkMiener <tjark.miener4692@googlemail.com>
2018-12-10 16:11:11 +0100

    bug fixed/now running with one channel

--- classification.py
+++ classification.py
@@ -20,12 +20,46 @@ from keras.layers import Dropout,Flatten,BatchNormalization,Activation
 from keras.layers import Convolution2D,MaxPooling2D,Dense
 from keras import optimizers
 
+def hotoneencoding(val):
+    arr = np.zeros((14,), dtype=int)
+    if val == 90:
+        arr[0] = 1
+    elif val == 42:
+        arr[1] = 1
+    elif val == 65:
+        arr[2] = 1
+    elif val == 16:
+        arr[3] = 1
+    elif val == 15:
+        arr[4] = 1
+    elif val == 62:
+        arr[5] = 1
+    elif val == 88:
+        arr[6] = 1
+    elif val == 92:
+        arr[7] = 1
+    elif val == 67:
+        arr[8] = 1
+    elif val == 95:
+        arr[9] = 1
+    elif val == 52:
+        arr[10] = 1
+    elif val == 6:
+        arr[11] = 1
+    elif val == 64:
+        arr[12] = 1
+    elif val == 53:
+        arr[13] = 1
+    else:
+        raise ValueError('Sorry! Class {} isn\'t supported.'.format(val))
+    return arr
+
 def neural_network(x_train,y_train,x_vali,y_vali):
     
     print('Build model...')
     model = Sequential()
             
-    model.add(Convolution2D(32, (3, 3),use_bias=False,input_shape=(5,150,150), data_format='channels_first'))
+    model.add(Convolution2D(32, (3, 3),use_bias=False,input_shape=(1,150,150), data_format='channels_first'))
     model.add(BatchNormalization())
     model.add(Activation("relu"))
     model.add(MaxPooling2D(pool_size=(2,2)))
@@ -41,7 +75,7 @@ def neural_network(x_train,y_train,x_vali,y_vali):
     model.add(Activation("relu"))
     #model.add(LeakyReLU(alpha=0.03))
     model.add(Dropout(0.25))
-    model.add(Dense(14, activation='sigmoid'))
+    model.add(Dense(14, activation='softmax'))
         
     model.compile(loss='categorical_crossentropy',
                           optimizer='adam',
@@ -50,11 +84,11 @@ def neural_network(x_train,y_train,x_vali,y_vali):
     model.summary()
                           
     print('Train...')
-    history = model.fit(x_train, y_train, validation_data=(x_vali, y_vali), epochs=15, batch_size=64)
+    history = model.fit(x_train, y_train, validation_data=(x_vali, y_vali), epochs=50, batch_size=64)
                           
     plt.figure(figsize=(12,8))
     
-    plt.subplot(221)
+    plt.subplot(211)
     plt.plot(history.history['acc'])
     plt.plot(history.history['val_acc'])
     plt.title('model train vs validation accuracy')
@@ -62,31 +96,16 @@ def neural_network(x_train,y_train,x_vali,y_vali):
     plt.xlabel('epoch')
     plt.legend(['train', 'validation'], loc='lower right')
         
-    plt.subplot(222)
+    plt.subplot(212)
     plt.plot(history.history['loss'])
     plt.plot(history.history['val_loss'])
     plt.title('model train vs validation loss')
     plt.ylabel('loss')
     plt.xlabel('epoch')
     plt.legend(['train', 'validation'], loc='upper right')
-            
-    plt.subplot(223)
-    resultsTrain = model.predict(x_train)
-    plt.scatter(range(871),resultsTrain,c='r')
-    plt.scatter(range(871),y_train,c='g')
-    #plt.legend(['prediction', 'real'], loc='upper right')
-    plt.title('Training prediction vs real')
-        
-            
-    plt.subplot(224)
-    resultsVali = model.predict(x_vali)
-    plt.scatter(range(109),resultsVali,c='r')
-    plt.scatter(range(109),y_vali,c='g')
-    #plt.legend(['prediction', 'real'], loc='upper right')
-    plt.title('Validation prediction vs real')
-        
+    
     plt.subplots_adjust(hspace=0.75)
-    plt.suptitle('LCC'.format(eps,steps),fontsize=14)
+    plt.suptitle('LCC',fontsize=14)
 
     #Saving the plots.
     plt.savefig('lossacc.png', dpi = 600)
@@ -108,13 +127,12 @@ def main():
     
     for i in np.arange(1,num_train+1,1):
         hdr = hdulTrain[i].header
-        x_train.append(hdulTrain[i].data)
+        x_train.append([hdulTrain[i].data[0]])
         redshift_train.append(hdr['PHOTOZ'])
         ra_train.append(hdr['RA'])
         dec_train.append(hdr['DECL'])
-        y_train.append(hdr['TARGET'])
+        y_train.append(hotoneencoding(hdr['TARGET']))
 
-    print(collections.Counter(y_train))
 
     num_vali = 785
     x_vali = []
@@ -125,14 +143,29 @@ def main():
     
     for i in np.arange(1,num_vali+1,1):
         hdr = hdulVali[i].header
-        x_vali.append(hdulVali[i].data)
+        x_vali.append([hdulVali[i].data[0]])
         redshift_vali.append(hdr['PHOTOZ'])
         ra_vali.append(hdr['RA'])
         dec_vali.append(hdr['DECL'])
-        y_vali.append(hdr['TARGET'])
+        y_vali.append(hotoneencoding(hdr['TARGET']))
+    
+    x_train = np.array(x_train)
+    y_train = np.array(y_train)
+    
+    x_vali = np.array(x_vali)
+    y_vali = np.array(y_vali)
+    print(np.array(x_train).shape)
+    print(np.array(x_vali).shape)
+    #y_train = np.expand_dims(y_train, axis=2)
+#y_vali = np.expand_dims(y_vali, axis=2)
 
-    print(collections.Counter(y_vali))
-
+    print(np.array(y_train).shape)
+    print(np.array(y_vali).shape)
+    #x_train = np.reshape(x_train,(7063,1,150,150,1))
+    #x_vali = np.reshape(x_vali,(785,1,150,150,1))
+    print(np.array(x_train).shape)
+    print(np.array(x_vali).shape)
+    
     neural_network(x_train,y_train,x_vali,y_vali)
 
     hdulVali.close()
